5eb59c6ef7dc0936f99d04096e01557c425a0c7d
zdychina <zdychina@sina.cn>
2019-07-05 23:23:19 +0800

    ww

--- src/com/atguigu/testgit/Employee.java
+++ src/com/atguigu/testgit/Employee.java
@@ -5,10 +5,7 @@ public class Employee {
 	private Integer id;
 	private String name;
 	private Integer age;
-<<<<<<< HEAD
 	private String address1133;
-=======
 	private String address1122;
->>>>>>> branch 'master' of git@github.com:UMR4J/TestGit01.git
 
 }
